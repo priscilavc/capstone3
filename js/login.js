b4988c4147a32bd9f08f04f294acf9b9f9bfccd1
paulasuncion <paulyasuncion1@gmail.com>
2023-01-17 10:10:00 -0800

    Linked all necessary pages

--- js/login.js
+++ js/login.js
@@ -22,3 +22,25 @@ loginForm.onsubmit = function (event) {
     // Time to actually process the login using the function from auth.js!
     login(loginData);
 };
+
+var myHeaders = new Headers();
+myHeaders.append("accept", "application/json");
+myHeaders.append("Content-Type", "application/json");
+myHeaders.append("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InN0cmluZyIsImlhdCI6MTY3Mzk3Njg4OCwiZXhwIjoxNjc0MDYzMjg4fQ.3K2dSF9T_peNjVTg3RWTg5FY0WyXu5rDKYB7wkPoB9o");
+
+var raw = JSON.stringify({
+  "username": "string",
+  "password": "string"
+});
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch("https://microbloglite.herokuapp.com/auth/login", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
