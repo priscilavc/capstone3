44fd2deb456215cd11f2f8893fa09ca68d47d73a
paulasuncion <paulyasuncion1@gmail.com>
2023-01-17 13:45:10 -0800

    more work

--- js/login.js
+++ js/login.js
@@ -2,7 +2,7 @@
 
 "use strict";
 
-const loginForm = document.querySelector("#login");
+const loginForm = document.getElementById("loginForm");
 
 loginForm.onsubmit = function (event) {
     // Prevent the form from refreshing the page,
@@ -17,30 +17,8 @@ loginForm.onsubmit = function (event) {
     }
 
     // Disables the button after the form has been submitted already:
-    loginForm.loginButton.disabled = true;
+    // loginForm.loginButton.disabled = true;
 
     // Time to actually process the login using the function from auth.js!
     login(loginData);
 };
-
-var myHeaders = new Headers();
-myHeaders.append("accept", "application/json");
-myHeaders.append("Content-Type", "application/json");
-myHeaders.append("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InN0cmluZyIsImlhdCI6MTY3Mzk3Njg4OCwiZXhwIjoxNjc0MDYzMjg4fQ.3K2dSF9T_peNjVTg3RWTg5FY0WyXu5rDKYB7wkPoB9o");
-
-var raw = JSON.stringify({
-  "username": "string",
-  "password": "string"
-});
-
-var requestOptions = {
-  method: 'POST',
-  headers: myHeaders,
-  body: raw,
-  redirect: 'follow'
-};
-
-fetch("https://microbloglite.herokuapp.com/auth/login", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
--- js/posts.js
+++ js/posts.js
@@ -63,25 +63,25 @@ function GetAllPost() {
 GetAllPost();
 
 
-//Pre-made code from starter template of project provided by REmsey
+// Pre-made code from starter template of project provided by REmsey
 // let myHeaders = new Headers();
-  // myHeaders.append(
-  //   "Authorization",
-  //   "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InN0cmluZyIsImlhdCI6MTY3MzM3Mjc1NiwiZXhwIjoxNjczNDU5MTU2fQ.4i8jrN6NXBa3C_9YUTCaYz6Fkz8kkKLUFNnNCLQPhFY"
-  // );
-  // myHeaders.append("Content-Type", "application/json");
+//   myHeaders.append(
+//     "Authorization",
+//     "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InN0cmluZyIsImlhdCI6MTY3MzM3Mjc1NiwiZXhwIjoxNjczNDU5MTU2fQ.4i8jrN6NXBa3C_9YUTCaYz6Fkz8kkKLUFNnNCLQPhFY"
+//   );
+//   myHeaders.append("Content-Type", "application/json");
 
-  // let raw = JSON.stringify({
-  //   text: "This is a post about JavaScript",
-  // });
+//   let raw = JSON.stringify({
+//     text: "This is a post about JavaScript",
+//   });
 
-  // let requestOptions = {
-  //   method: "GET",
-  //   headers: myHeaders,
-  // };
+//   let requestOptions = {
+//     method: "GET",
+//     headers: myHeaders,
+//   };
 
-  // let myresults = document.getElementById("results");
-  // fetch("https://microbloglite.herokuapp.com/api/posts", requestOptions)
-  //   .then((response) => response.text())
-  //   .then((result) => (myresults.innerHTML += result))
-  //   .catch((error) => console.log("error", error));
+//   let myresults = document.getElementById("results");
+//   fetch("https://microbloglite.herokuapp.com/api/posts", requestOptions)
+//     .then((response) => response.text())
+//     .then((result) => (myresults.innerHTML += result))
+//     .catch((error) => console.log("error", error));
