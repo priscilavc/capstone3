93818b65c7919f5d3ee9af241f41366cf2c907e4
paulasuncion <paulyasuncion1@gmail.com>
2023-01-18 14:06:09 -0800

    Registration js final

--- js/registration.js
+++ js/registration.js
@@ -1,37 +1,40 @@
 const form = document.getElementById('registrationForm');
 
 function registerUser(event) {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-    const cpassword = document.getElementById('cpassword').value;
+    const usernameInput = document.getElementById('username').value;
+    const fullNameInput = document.getElementById('fullname').value;
+    const passwordInput = document.getElementById('password').value;
+    const cpasswordInput = document.getElementById('cpassword').value;
 
     event.preventDefault();
 
-    let myHeaders = new Headers();
+    var myHeaders = new Headers();
     myHeaders.append("accept", "application/json");
     myHeaders.append("Content-Type", "application/json");
-    myHeaders.append("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InN0cmluZyIsImlhdCI6MTY3MzYzNjQwMywiZXhwIjoxNjczNzIyODAzfQ.UqiYDT72durosYvJAPLiEEPGWArDEhsBNlbd-Wjkcfc");
-
-    let raw = JSON.stringify({
-    "username": username,
-    "fullname": password,
-    "password": cpassword,
-    });
-
-    let requestOptions = {
-    method: 'POST',
-    headers: myHeaders,
-    body: raw,
-    redirect: 'follow'
+    
+    var raw = JSON.stringify({
+      "username": usernameInput,
+      "fullName": fullNameInput,
+      "password": passwordInput,
+      "cpassword": cpasswordInput,
+        });
+    
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
     };
-
+    
     fetch("https://microbloglite.herokuapp.com/api/users", requestOptions)
-    .then(response => response.text())
-    .then(result => 
-        {
-            alert(result);
-            console.log(result);
-        })
-        
-    }
-    form.addEventListener('submit' , registerUser)
+      .then(response => response.text())
+      .then(result => {
+        console.log(result)
+        window.location.replace("login.html")
+      })
+      .catch(error => console.log('error', error));
+
+
+}
+
+form.addEventListener('submit', registerUser);
